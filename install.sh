5683c5d730effeb70041d191bfd91cfb3f6f37f6
fraserweist <fraserweist@gmail.com>
2017-08-07 13:32:49 -0400

    renamed some stuff

--- install.sh
+++ install.sh
@@ -3,25 +3,20 @@
 dir=~/dotfiles
 olddir=~/dotfiles_old
 folders="applescripts bash iterm2 mutt screen tmux vim"
-files="applescripts bashrc bash_profile vimrc vim screenrc screen_layout tmux.conf tmux muttrc mutt iterm2"
+files="applescripts bashrc bash_profile vimrc vim screenrc screen-layout tmux.conf tmux muttrc mutt iterm2"
 
 ##########
 
 # create dotfiles_old in homedir
-echo "creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
-echo "changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
-    echo "creating symlink to $file in home directory."
+    echo "linking $file in home directory..."
     ln -s $dir/$file ~/.$file
 done
 
