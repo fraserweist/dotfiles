84be850daed9d3e2962edca93d31b2e0070bf085
fraserweist <fraserweist@gmail.com>
2017-08-12 18:21:11 -0400

    install script is so pretty!

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+echo ------------FOLDERS------------
+
 for folder in *; do
-    if [ "$folder" = "install.sh" ]; then
-        echo nope
-    elif [ "$folder" = "runcom" ]; then
-        echo nope
-    else
-        ln -s ~/dotfiles/$folder ~/.$folder
+    if [ "$folder" != "install.sh" ] && [ "$folder" != "runcom" ]; then
+        ln -s ~/dotfiles/$folder ~
+        rm ~/.$folder
+        mv ~/$folder ~/.$folder
         echo "linking $folder in ~"
     fi
 done
 
+echo -------------FILES-------------
+
 cd runcom
 for file in *; do
-    ln -s ~/dotfiles/runcom/$file ~/.$file
+    ln -s ~/dotfiles/runcom/$file ~
+    rm ~/.$file
+    mv ~/$file ~/.$file
     echo "linking $file in ~"
 done
 
