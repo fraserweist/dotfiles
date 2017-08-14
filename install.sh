19fdadfcff463870a71d17a499fc23a963007875
fraserweist <fraserweist@gmail.com>
2017-08-13 20:47:47 -0400

    install script is prettier, deleted recursive symlinks

--- install.sh
+++ install.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 
-echo ------------FOLDERS------------
+echo -e "\n\033[1;34m                         fraser weist's \033[1;35m"
+echo "          ___            ___    _____  ___  ___"
+echo "    _____/  / ______    /  /_  /  __/ /__/ /  / ____    ______"
+echo "   /  __   / /  __  \  /  __/ /  /_  ___  /  / / __ \  /  ___/"
+echo "  /  /_/  / /  /_/  / /  /_  /  __/ /  / /  / /  ___/ /___  /"
+echo "  \______/  \______/  \___/ /__/   /__/ /__/  \____/ /_____/"
+echo -e "\n\033[1;30m           (https://github.com/fraserweist/dotfiles)"
+
+echo -e "\n\033[1;31m  FOLDERS:\033[0;31m"
 
 for folder in *; do
     if [ "$folder" != "install.sh" ] && [ "$folder" != "runcom" ]; then
         ln -s ~/dotfiles/$folder ~
         rm ~/.$folder
         mv ~/$folder ~/.$folder
-        echo "linking $folder in ~"
+        echo -e "    linking $folder at ~/.$folder/"
     fi
 done
 
-echo -------------FILES-------------
+echo -e "\n\033[1;33m  FILES:\033[0;33m"
 
 cd runcom
 for file in *; do
     ln -s ~/dotfiles/runcom/$file ~
     rm ~/.$file
     mv ~/$file ~/.$file
-    echo "linking $file in ~"
+    echo -e "    linking $file at ~/.$file"
 done
+
+echo " "
 
 # vim set syn=sh
