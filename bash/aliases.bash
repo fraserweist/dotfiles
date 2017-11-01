8825a984ff29252d1b7d6fb19a1bed5039f638a1
fraserweist <fraserweist@gmail.com>
2017-11-01 17:32:20 -0400

    changed vim 8.0 thing

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -3,9 +3,6 @@ alias ls='ls -Gh'
 alias la='ls -A'
 alias ll='ls -alF'
 
-# vim 8.0
-alias vim=/usr/local/Cellar/vim/8.0.0844/bin/vim
-
 if [ "$platform" = "linux" ]; then
     alias subl="/opt/Sublime\ Text\ 2/sublime_text"
 elif [ "$platform" = "mac" ]; then
