ebf3e78593df594abffd342d9af558bae4aaf255
Fraser Weist <fraserweist@gmail.com>
2018-12-06 10:46:07 -0500

    added scripts to dotfiles, updated install script, adjusted bb-tmux-env

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ echo -e "  \______/  \______/  \___/ /__/   /__/ /__/  \____/ /_____/ /__/"
 echo -e "\n\033[1;30m"
 echo -e "           (https://github.com/fraserweist/dotfiles)"
 
-echo -e "\n\033[1;31m  FOLDERS:\033[0;31m"
+
+root=$(pwd)
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
@@ -20,23 +21,45 @@ case "${unameOut}" in
     *)          machhine=Other;;
 esac
 
+echo -e "\n\033[1;31m  FOLDERS:\033[0;31m"
 for folder in *; do
-    if [ "$folder" != "install.sh" ] && [ "$folder" != "runcom" ]; then
-        ln -s ~/dotfiles/$folder ~
-        rm ~/.$folder
-        mv ~/$folder ~/.$folder
-        echo -e "    linking $folder at ~/.$folder/"
+    if [ "$folder" != "install.sh" ] && [ "$folder" != "runcom" ] && \
+      [ "$folder" != "scripts" ]; then
+        ln -s $root/$folder $HOME
+        rm $HOME/.$folder
+        mv $HOME/$folder $HOME/.$folder
+        echo -e "    linking $folder at $HOME/.$folder/"
     fi
 done
 
-echo -e "\n\033[1;33m  FILES:\033[0;33m"
-
+echo -e "\n\033[1;33m  RC FILES:\033[0;33m"
 cd runcom
 for file in *; do
-    ln -s ~/dotfiles/runcom/$file ~
-    rm ~/.$file
-    mv ~/$file ~/.$file
-    echo -e "    linking $file at ~/.$file"
+    ln -s $root/runcom/$file $HOME
+    rm $HOME/.$file
+    mv $HOME/$file $HOME/.$file
+    echo -e "    linking $file at $HOME/.$file"
+done
+
+echo -e "\n\033[1;32m  SCRIPTS:\033[0;32m"
+echo -e "    (recreating $HOME/bin/)"
+rm -rf ~/bin
+mkdir ~/bin
+
+if [[ ":$PATH:" == *":$HOME/bin:"* ]]; then
+    echo -e "    ($HOME/bin is already in PATH)"
+else
+    echo -e "    ($HOME/bin isn't in PATH, adding it)"
+    PATH=$PATH:$HOME/bin
+    export PATH
+fi
+
+
+cd ../scripts
+for file in *; do
+    ln -s $root/scripts/$file $HOME/bin
+    echo -e "    adding $file to $HOME/bin/"
+    chmod +x $HOME/bin/$file
 done
 
 echo " "
