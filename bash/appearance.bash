90f7ff6c45b39c2de4d5df2d1b10d1e4dab0e35d
Fraser Weist <fweist@bloomberg.net>
2019-12-05 10:40:51 -0500

    remove secrets ref, refactor appearance.bash to use $(color)

--- bash/appearance.bash
+++ bash/appearance.bash
@@ -1,25 +1,14 @@
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
-
-# colors
-YELLOW="\[\033[33m\]"
-GREEN="\[\033[01;42;97m\]"
-GREEN_TO_BLUE="\[\033[0;32;44m\]"
-RED="\[\033[1;31m\]"
-BLUE="\[\033[1;44;97m\]"
-BLUE_TO_PURPLE="\[\033[;34;45m\]"
-PURPLE="\[\033[1;45;97m\]"
-PURPLE_TO_RESET="\[\033[0;35;49m\]"
-WHITE="\[\033[1;107;30m\]"
 RESET="\[\033[00m\]"
-SEP=""
 
 parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-PS1="${debian_chroot:+($debian_chroot)}${GREEN} \u ${GREEN_TO_BLUE}${BLUE} \w ${BLUE_TO_PURPLE}${PURPLE}\$(parse_git_branch) ${PURPLE_TO_RESET}${RESET}\n>> "
+user_section="$(color style:bold,fg:light_grey,bg:green) \u "
+dir_section="$(color style:bold,fg:light_grey,bg:blue) \w "
+git_section="$(color style:bold,fg:light_grey,bg:purple)\$(parse_git_branch) "
 
-# for ls colors
-export LS_COLORS='fi=1;95:di=1;94:ex=1;31:ln=target'
+final_prompt="${RESET}\n>> "
+
+PS1="${user_section}${dir_section}${git_section}${final_prompt}"
+
+export LS_COLORS='fi=1;93:di=1;96:ex=1;31:ln=target'
